3e63c070e5e9b47c702f536c8803b6bb30acfeb5
Robinovich <andrew.podobed@gmail.com>
2021-03-17 13:31:26 +0300

    This is a Java program for reversing a sequence of characters in a string without using the String built-in reverse () function.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# taskReverseSequence" 
+"# taskReverseSequence"
+
+This is a Java program for reversing a sequence 
+of characters in a string without using the String 
+built-in reverse () function.
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,5 +10,20 @@ public class Main {
 	for(int i = sym.length - 1;i >= 0; i--){
         System.out.print(sym[i]);
     }
+        System.out.println("");
+	/*
+	second method
+	 */
+        String str2;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your string");
+        str2 = scan.nextLine();
+        String [] arr = str2.split("");
+        for( int j = arr.length - 1; j >= 0; j--){
+            System.out.print(arr[j] + " ");
+
+        }
+
+
     }
 }
